d62f408a40f0250f47e5e0b44371adb6bc5e63f9
양은수 <93758339+diddmstn@users.noreply.github.com>
2024-05-30 15:08:34 +0900

    >> Fix GameItem

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MovingPlatform : MonoBehaviour
 {
@@ -18,14 +20,16 @@ public class MovingPlatform : MonoBehaviour
         {
             if( Vector3.Distance(transform.localPosition, pos[curPos].transform.localPosition) <= 1f)
             {
-                if(curPos>pos.Count)
+                 curPos++;
+                if(curPos>=pos.Count)
                 {
-                    end=true;
-                    return;
+                    curPos=pos.Count-1;
                 }
-                curPos++;
             }
-            transform.localPosition = Vector3.Lerp(transform.localPosition,pos[curPos].transform.localPosition, speed * Time.deltaTime);
+            else
+            {
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition,pos[curPos].transform.localPosition, speed * Time.deltaTime);
+            }
         }
         else
         {
@@ -41,16 +45,16 @@ public class MovingPlatform : MonoBehaviour
         {
             StopCoroutine(coroutine);
         }
-        other.transform.parent = this.transform;
+        other.transform.SetParent(this.transform);
         inPlatform=true;
 
-        //플레이어를 자식으로 넣기
     }
     private void OnCollisionExit(Collision other) 
     {
         coroutine = StartCoroutine(Count());
-        inPlatform=false;
-        other.transform.parent = null;
+        other.transform.SetParent(null);
+        DontDestroyOnLoad(other.gameObject);
+
     }
 
     IEnumerator Count()
@@ -59,6 +63,7 @@ public class MovingPlatform : MonoBehaviour
 
         inPlatform=false;
         end=false;
+        curPos=1;
 
     }
 
